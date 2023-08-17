7b4092c487e25d80baf80beabf99bef1a1519867
Maddala-Anusha <110710962+Maddala-Anusha@users.noreply.github.com>
2023-08-17 17:01:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+
+	$(function() {
+		var header = $(".start-style");
+		$(window).scroll(function() {    
+			var scroll = $(window).scrollTop();
+		
+			if (scroll >= 10) {
+				header.removeClass('start-style').addClass("scroll-on");
+			} else {
+				header.removeClass("scroll-on").addClass('start-style');
+			}
+		});
+	});		
+		
+	//Animation
+	
+	$(document).ready(function() {
+		$('body.hero-anime').removeClass('hero-anime');
+	});
+
+	//Menu On Hover
+		
+	$('body').on('mouseenter mouseleave','.nav-item',function(e){
+			if ($(window).width() > 750) {
+				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+				setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+				},1);
+			}
+	});	
+	
+	//Switch light/dark
+	
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("dark")) {
+			$("body").removeClass("dark");
+			$("#switch").removeClass("switched");
+			$(".navbar-brand").css("color","black");
+		}
+		else {
+			$("body").addClass("dark");
+			$("#switch").addClass("switched");
+			$(".navbar-brand").css("color","white");
+		}
+	});  
+	
+  })(jQuery);
+
+
+  $(document).ready(function(){
+	var zindex = 10;
+	
+	$("div.card").click(function(e){
+	  e.preventDefault();
+  
+	  var isShowing = false;
+  
+	  if ($(this).hasClass("show")) {
+		isShowing = true
+	  }
+  
+	  if ($("div.cards").hasClass("showing")) {
+		// a card is already in view
+		$("div.card.show")
+		  .removeClass("show");
+  
+		if (isShowing) {
+		  // this card was showing - reset the grid
+		  $("div.cards")
+			.removeClass("showing");
+		} else {
+		  // this card isn't showing - get in with it
+		  $(this)
+			.css({zIndex: zindex})
+			.addClass("show");
+  
+		}
+  
+		zindex++;
+  
+	  } else {
+		// no cards in view
+		$("div.cards")
+		  .addClass("showing");
+		$(this)
+		  .css({zIndex:zindex})
+		  .addClass("show");
+  
+		zindex++;
+	  }
+	  
+	});
+  });
